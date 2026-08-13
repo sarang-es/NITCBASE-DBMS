8b71679c8ed09b35b894602afe9ed83d8b36dbd6
sarang-es <sarangcodes0.5@gmail.com>
2026-08-13 23:30:10 +0530

    Stage - 2 Record Blocks & Catalogs - Printing Relations & Attributes using getheader(), getrecord().

--- mynitcbase/Buffer/BlockBuffer.cpp
+++ mynitcbase/Buffer/BlockBuffer.cpp
@@ -1,1 +1,82 @@
 #include "BlockBuffer.h"
+#include "../Disk_Class/Disk.h"
+#include <cstring>
+
+
+// Constructor
+BlockBuffer::BlockBuffer(int blockNum) {
+    this->blockNum = blockNum;
+}
+
+
+// Calls parent class constructor
+RecBuffer::RecBuffer(int blockNum)
+    : BlockBuffer(blockNum) {
+}
+
+
+// Load the block header into *head
+int BlockBuffer::getHeader(struct HeadInfo *head) {
+
+    unsigned char buffer[BLOCK_SIZE];
+
+    // Read the block into buffer
+    int status = Disk::readBlock(buffer, this->blockNum);
+
+    if (status != SUCCESS) {
+        return status;
+    }
+
+    // Extract header fields
+    memcpy(&head->blockType,  buffer + 0,  4);
+    memcpy(&head->pblock,     buffer + 4,  4);
+    memcpy(&head->lblock,     buffer + 8,  4);
+    memcpy(&head->rblock,     buffer + 12, 4);
+    memcpy(&head->numEntries, buffer + 16, 4);
+    memcpy(&head->numAttrs,   buffer + 20, 4);
+    memcpy(&head->numSlots,   buffer + 24, 4);
+    memcpy(head->reserved,    buffer + 28, 4);
+
+    return SUCCESS;
+}
+
+
+// Load record at slotNum into rec
+int RecBuffer::getRecord(union Attribute *rec, int slotNum) {
+
+    struct HeadInfo head;
+
+    // Get header information
+    int status = this->getHeader(&head);
+
+    if (status != SUCCESS) {
+        return status;
+    }
+
+    int attrCount = head.numAttrs;
+    int slotCount = head.numSlots;
+
+    unsigned char buffer[BLOCK_SIZE];
+
+    // Read block into buffer
+    status = Disk::readBlock(buffer, this->blockNum);
+
+    if (status != SUCCESS) {
+        return status;
+    }
+
+    // Calculate record size
+    int recordSize = attrCount * ATTR_SIZE;
+
+    // Find the beginning of the required record
+    unsigned char *slotPointer =
+        buffer +
+        HEADER_SIZE +
+        slotCount +
+        (recordSize * slotNum);
+
+    // Copy record into rec
+    memcpy(rec, slotPointer, recordSize);
+
+    return SUCCESS;
+}
--- mynitcbase/main.cpp
+++ mynitcbase/main.cpp
@@ -1,27 +1,51 @@
 #include "Disk_Class/Disk.h"
 #include "Buffer/BlockBuffer.h"
+#include <cstring>
 #include <iostream>
 using namespace std;
 
-int main() {
+int main(int argc, char *argv[]) {
 
-    Disk disk;
+    Disk disk_run;
 
-    unsigned char buffer[BLOCK_SIZE];
+    // Create objects for Relation Catalog and Attribute Catalog
+    RecBuffer relCatBuffer(RELCAT_BLOCK);
+    RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
 
-    int status = Disk::readBlock(buffer, 0);
+    HeadInfo relCatHeader;
+    HeadInfo attrCatHeader;
 
-    if(status != SUCCESS) {
-        cout << "Read failed\n";
-        return 1;
-    }
+    // Load headers
+    relCatBuffer.getHeader(&relCatHeader);
+    attrCatBuffer.getHeader(&attrCatHeader);
 
-    for(int i = 0; i < BLOCK_SIZE; i++) {
+    // Traverse all relations in RELCAT
+    for (int i = 0; i < relCatHeader.numEntries; i++) {
 
-        cout << (int)buffer[i] << " ";
+        Attribute relCatRecord[RELCAT_NO_ATTRS];
 
-        if((i + 1) % 32 == 0)
-            cout << endl;
+        relCatBuffer.getRecord(relCatRecord, i);
+
+        cout<<"Relation: "<<relCatRecord[RELCAT_REL_NAME_INDEX].sVal<<endl;
+
+        // Traverse all attributes in ATTRCAT
+        for (int j = 0; j < attrCatHeader.numEntries; j++) {
+
+            Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+
+            attrCatBuffer.getRecord(attrCatRecord, j);
+
+            // Check whether this attribute belongs
+            // to the current relation
+            if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal,relCatRecord[RELCAT_REL_NAME_INDEX].sVal) == 0) {
+
+                const char *attrType =attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal== NUMBER? "NUM": "STR";
+
+                cout<<"  "<<attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal<<": "<<attrType<<endl;
+            }
+        }
+
+        cout<<endl;
     }
 
     return 0;
