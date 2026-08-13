b7334e5cdfd75713ee7cf89a995505f03038be8a
sarang-es <sarangcodes0.5@gmail.com>
2026-08-13 22:33:49 +0530

    Stage - 1_Exercise -1 Printing Block-0 of Block Buffer Map

--- mynitcbase/main.cpp
+++ mynitcbase/main.cpp
@@ -1,6 +1,7 @@
 #include "Disk_Class/Disk.h"
 #include "Buffer/BlockBuffer.h"
 #include <iostream>
+using namespace std;
 
 int main() {
 
@@ -10,11 +11,17 @@ int main() {
 
     int status = Disk::readBlock(buffer, 0);
 
-    if(status == SUCCESS) {
-        std::cout << "Block 0 read successfully\n";
+    if(status != SUCCESS) {
+        cout << "Read failed\n";
+        return 1;
     }
-    else {
-        std::cout << "Error reading block\n";
+
+    for(int i = 0; i < BLOCK_SIZE; i++) {
+
+        cout << (int)buffer[i] << " ";
+
+        if((i + 1) % 32 == 0)
+            cout << endl;
     }
 
     return 0;
